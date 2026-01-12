12dc76a589bf923b13ba9041242a42509bdfe2d1
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-01-12 17:27:17 -0500

    feat: replace app icon and rename to Time Tracker for Android

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Time Tracker',
       theme: ThemeData(useMaterial3: true),
       home: const TimeTrackerScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
